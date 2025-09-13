ef3100a7d8a1544f0eb881ffaad2d91f53139a69
Anvar Zaripboyev <58693223+atezkor@users.noreply.github.com>
2025-09-13 10:22:50 +0500

    Update RegionEntity.cs

--- ORS/apps/regions/Entities/RegionEntity.cs
+++ ORS/apps/regions/Entities/RegionEntity.cs
@@ -4,28 +4,27 @@ namespace ORS.Apps.Regions.Entities;
 
 public class RegionEntity() {
     public int id { get; set; }
-
-    public string name { get; set; }
     
     public int? ParentId { get; set; }
     public int Order { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
 
-    public Dictionary<string, string> names { get; } = new();
+    public Dictionary<string, string> Name { get; } = new();
     public Dictionary<string, string> ShortName { get; set; } = new();
 
     public RegionEntity(Region region) : this() {
         id = region.Id;
-        name = region.Name;
         ParentId = region.ParentId;
         Order = region.Order;
         Latitude = region.Latitude;
         Longitude = region.Longitude;
         
-        names["uzl"] = region.NameUzl;
-        names["uzk"] = region.NameUzk;
-        names["ru"] = region.NameRu;
+        Name["uz"] = region.NameUzl;
+        Name["uzk"] = region.NameUzk;
+        Name["ru"] = region.NameRu;
+        Name["en"] = region.NameUzk;
+        Name["kaa"] = region.NameUzk;
 
         ShortName = region.ShortName;
     }
